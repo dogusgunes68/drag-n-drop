a2ba94958ebbe9ab036d3146d05f8917540d208d
dogusgunes68 <dogusgunes68@gmail.com>
2023-04-24 17:38:30 +0300

    drag n drop done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import "./App.css";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 import ToDoList from "./ToDoList";
 import "antd/dist/reset.css";
+import DragDrop from "./DragDrop";
 
 function App() {
   return (
     <div className="App">
-      <ToDoList />
+      <DragDrop />
     </div>
   );
 }
